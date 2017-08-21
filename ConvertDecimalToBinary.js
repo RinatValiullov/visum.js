27bafed3bf8d3fed25d5c748b725629a2fd32d5d
4rontender <4rontender@gmail.com>
2017-08-21 11:50:03 +0500

    add some helper file

---
+++ ConvertDecimalToBinary.js
@@ -0,0 +1,13 @@
+``` javascript
+  var convertDecimalToBinary = function (decimal) {
+    var binaryBase = 2;
+    var binary = [];
+    var remainder;
+    while (decimal !== 0) {
+        remainder = decimal % binaryBase;
+        decimal = Math.floor(decimal/binaryBase);
+        binary.unshift(remainder);
+    }
+    return binary.join("")
+};
+```
